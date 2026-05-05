cb93c2d645cd7e26042dd84cb42122f47e987e1d
lojoeugenio4-max <lojoeugenio4@gmail.com>
2026-05-05 14:17:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// 👉 Esto evita el zoom en móvil
+let viewport = document.querySelector("meta[name=viewport]");
+
+if (!viewport) {
+  viewport = document.createElement("meta");
+  viewport.setAttribute("name", "viewport");
+  document.head.appendChild(viewport);
+}
+
+viewport.setAttribute(
+  "content",
+  "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+);
+
+createRoot(document.getElementById("root")).render(<App />);import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
 createRoot(document.getElementById("root")).render(<App />);
